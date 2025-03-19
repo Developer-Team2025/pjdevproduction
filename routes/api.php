c9ca353fa493db614221ee5e8cf5b20f416c9d8a
Joben0819 <neboj08@gmail.com>
2025-03-19 14:58:23 +0800

    sample

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\CertificateController;
 use App\Http\Controllers\API\GoogleSheetsApiController;
 use App\Http\Controllers\API\OurTeamsController;
+use App\Http\Controllers\API\SampleRequest;
 use Illuminate\Support\Facades\Route;
 
 // Googlesheets API
@@ -12,5 +13,6 @@ Route::post('/google-api-create-row', [GoogleSheetsApiController::class, 'writeS
 Route::post('/create-team-profile', [OurTeamsController::class, 'createOurTeamProfile']);
 Route::get('/our-teams', [OurTeamsController::class, 'getOurTeams']);
 Route::get('/certificates', [CertificateController::class, 'getCertificates']);
+Route::post('/sample', [SampleRequest::class, 'sample']);
 
 // Edit Routes
